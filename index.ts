b6d69fb323be76ac1ed4f6b29ca707811dc261b9
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2025-10-03 02:23:39 +0000

    move report functions to their own file

--- index.ts
+++ index.ts
@@ -1,33 +1,18 @@
 import os = require("os");
 import ts = require("typescript");
-import * as fs from 'fs';
-import * as path from 'path';
-import csv from 'csv-parser';
-import { log } from "console";
-import { resourceUsage } from "process";
 import prompt from 'prompt-sync';
+import * as path from 'path';
+import * as fs from 'fs';
+import { getTotalProtein, getTotalTodayProtein } from './report.js';
 
-interface ProteinAmount {
+
+export interface ProteinAmount {
     loggedDate: string;
     amount: number;
 }
 
-function todayDate(): string {
+export function todayDate(): string {
     return Date().substring(4,15);
-}
-
-async function getTotalTodayProtein(){
-    const csvFile = path.join("proteinRecord.csv");
-    const amountReport: Map<string,number> = await getReportCSV(csvFile);
-    console.log(`Your today protein consumption is ${amountReport.get(todayDate())}`);
-}
-
-async function getTotalProtein(){
-    const csvFile = path.join("proteinRecord.csv");
-    const amountReport: Map<string,number> = await getReportCSV(csvFile);
-    amountReport.forEach((amount, loggedDate) => {
-        console.log(`${loggedDate}: ${amount}`)
-    });
 }
 
 const proteinAmountList: ProteinAmount[] = [];
@@ -51,29 +36,6 @@ function writeToCsv(data: ProteinAmount): void {
     } catch (error) {
         console.error("Fail log to csv.");
     }
-}
-
-function getReportCSV(csvFile: string): Promise<Map<string, number>> {
-    return new Promise((resolve,reject) => {
-        const overalReport: Map<string, number> = new Map<string, number>();
-        fs.createReadStream(csvFile)
-        .pipe(csv())
-        .on('data', (row: ProteinAmount) => {
-            const amountColumn: Number = row.amount;
-            const loggedDateColumn: string = row.loggedDate;
-            const currentAmount: Number = overalReport.get(loggedDateColumn) ?? 0;
-            overalReport.set(loggedDateColumn, Number(amountColumn)! + Number(currentAmount));
-            
-        })
-        .on('end', () => {
-            console.log('CSV successfully processed.');
-            resolve(overalReport);
-        })
-        .on('error', (err) => {
-            console.error('Error reading or parsing CSV:', err);
-            reject(err);
-        });
-    })
 }
 
 // --- Execution function ---
---
+++ report.ts
@@ -0,0 +1,42 @@
+import * as path from 'path';
+import {todayDate} from './index.js';
+import * as fs from 'fs';
+import csv from 'csv-parser';
+import type { ProteinAmount } from './index.js';
+
+export async function getTotalTodayProtein(){
+    const csvFile = path.join("proteinRecord.csv");
+    const amountReport: Map<string,number> = await getReportCSV(csvFile);
+    console.log(`Your today protein consumption is ${amountReport.get(todayDate())}`);
+}
+
+export async function getTotalProtein(){
+    const csvFile = path.join("proteinRecord.csv");
+    const amountReport: Map<string,number> = await getReportCSV(csvFile);
+    amountReport.forEach((amount, loggedDate) => {
+        console.log(`${loggedDate}: ${amount}`)
+    });
+}
+
+function getReportCSV(csvFile: string): Promise<Map<string, number>> {
+    return new Promise((resolve,reject) => {
+        const overalReport: Map<string, number> = new Map<string, number>();
+        fs.createReadStream(csvFile)
+        .pipe(csv())
+        .on('data', (row: ProteinAmount) => {
+            const amountColumn: Number = row.amount;
+            const loggedDateColumn: string = row.loggedDate;
+            const currentAmount: Number = overalReport.get(loggedDateColumn) ?? 0;
+            overalReport.set(loggedDateColumn, Number(amountColumn)! + Number(currentAmount));
+            
+        })
+        .on('end', () => {
+            console.log('CSV successfully processed.');
+            resolve(overalReport);
+        })
+        .on('error', (err) => {
+            console.error('Error reading or parsing CSV:', err);
+            reject(err);
+        });
+    })
+}
